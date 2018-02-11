69bedd107686b228a9982add9a9d838cabb9f646
ChipLeo <winleo@ua.fm>
2018-02-11 23:40:29 +0200

    Added INHABIT_ROOT

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -440,6 +440,8 @@ bool Creature::UpdateEntry(uint32 Entry, uint32 team, const CreatureData* data)
         ApplySpellImmune(0, IMMUNITY_EFFECT, SPELL_EFFECT_ATTACK_ME, true);
     }
 
+    if (cInfo->InhabitType & INHABIT_ROOT)
+        SetControlled(true, UNIT_STATE_ROOT);
     UpdateMovementFlags();
     return true;
 }
